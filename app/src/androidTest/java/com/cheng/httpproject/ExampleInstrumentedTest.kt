5335fa57b98047f2b1d7198edc1b84667c11a103
Chandler Cheng <chandler.cheng@plexure.com>
2019-10-15 13:57:56 +1300

    Update androidTest

--- app/src/androidTest/java/com/cheng/httpproject/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/cheng/httpproject/ExampleInstrumentedTest.kt
@@ -1,24 +1,22 @@
 package com.cheng.httpproject
 
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.cheng.httpproject", appContext.packageName)
     }
 }
