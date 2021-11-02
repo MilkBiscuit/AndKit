6f375bdb5ee21ef9affb16757ef3ed3276d46092
Chandler Cheng <dc9909@hotmail.com>
2021-11-02 14:20:15 +1300

    Open method createNotificationChannelIfNotExist and isNotificationEnabled

--- apiKit/src/main/java/com/cheng/apikit/util/NotificationUtil.kt
+++ apiKit/src/main/java/com/cheng/apikit/util/NotificationUtil.kt
@@ -3,7 +3,6 @@ package com.cheng.currencyalert
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -18,9 +17,11 @@ object NotificationUtil {
     private const val NOTIFICATION_CHANNEL_ID = "ApiKit"
     private const val NOTIFICATION_CHANNEL_NAME = "Notification"
 
-    fun showNotification(context: Context, notificationTitle: String, notificationText: String, smallIcon: IconCompat? = null) {
-        createNotificationChannelIfNecessary(context)
-        if (!isNotificationEnabled(context)) {
+    fun showNotification(
+        context: Context, notificationTitle: String, notificationText: String, smallIcon: IconCompat? = null
+    ) {
+        createNotificationChannelIfNotExist(context, NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME)
+        if (!isNotificationEnabled(context, NOTIFICATION_CHANNEL_ID)) {
             Log.e(TAG, "Notification is not enabled, please double check the permission.")
 
             return
@@ -42,13 +43,13 @@ object NotificationUtil {
         manager.notify(notificationId, notificationBuilder.build())
     }
 
-    private fun createNotificationChannelIfNecessary(context: Context) {
+    fun createNotificationChannelIfNotExist(context: Context, channelId: String, channelName: String) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val existingChannel = manager.getNotificationChannel(NOTIFICATION_CHANNEL_ID)
+            val existingChannel = manager.getNotificationChannel(channelId)
             if (existingChannel == null) {
-                val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID,
-                    NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_DEFAULT
                 )
                 channel.enableVibration(false)
                 manager.createNotificationChannel(channel)
@@ -56,11 +57,11 @@ object NotificationUtil {
         }
     }
 
-    private fun isNotificationEnabled(context: Context): Boolean {
+    fun isNotificationEnabled(context: Context, channelId: String): Boolean {
         val notificationEnabled = NotificationManagerCompat.from(context).areNotificationsEnabled()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val channel = manager.getNotificationChannel(NOTIFICATION_CHANNEL_ID) ?: return false
+            val channel = manager.getNotificationChannel(channelId) ?: return false
 
             return channel.importance != NotificationManager.IMPORTANCE_NONE && notificationEnabled
         }
